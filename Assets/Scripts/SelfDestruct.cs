4d4ad3a565e494746babbf8c641b3e18aeead0b7
carnoprof <carnovalefrancescodavide@asproni-fermi.edu.it>
2024-11-21 19:06:52 +0100

    Cleaning up stray bullets, setting self destruct after 2 seconds

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float destroyTime;
+    public GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(self, destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
